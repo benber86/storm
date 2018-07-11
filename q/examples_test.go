7383b9a1b62b6974c5311255796189bffaa319ba
benber86 <benoitb@tutanota.de>
2018-07-12 05:57:06 +0900

    Adding test for Custom Type Query

--- q/examples_test.go
+++ q/examples_test.go
@@ -1,6 +1,7 @@
 package q_test
 
 import (
+	"errors"
 	"fmt"
 	"log"
 
@@ -36,6 +37,47 @@ func ExampleRe() {
 	// Found 2 users.
 }
 
+func ExampleCustom() {
+	dir, db := prepareDB()
+	defer os.RemoveAll(dir)
+	defer db.Close()
+
+	var users []User
+	testTime := NullTime{time.Now().AddDate(0, -1*2, 0), true}
+	if err := db.Select(q.Gte("UpdatedAt", testTime)).Find(&users); err != nil {
+		fmt.Println("error: Select failed:", err)
+		return
+	}
+	fmt.Println("Found", len(users), "users.")
+
+	// Output:
+	// Found 2 users.
+}
+
+type NullTime struct {
+	Time  time.Time
+	Valid bool // Valid is true if Time is not NULL
+}
+
+func (n NullTime) Compare(other interface{}) (int, error) {
+	o, ok := other.(NullTime)
+	if !ok {
+		return 0, errors.New("Can only compare with variable of type NullTime")
+	}
+	if !n.Valid && !o.Valid {
+		return 0, nil
+	}
+	if n.Valid && o.Valid {
+		if n.Time.Equal(o.Time) {
+			return 0, nil
+		} else if n.Time.Before(o.Time) {
+			return -1, nil
+		}
+		return 1, nil
+	}
+	return 0, errors.New("Can not compare Null Time and non Null Time")
+}
+
 type User struct {
 	ID        int    `storm:"id,increment"`
 	Group     string `storm:"index"`
@@ -43,6 +85,7 @@ type User struct {
 	Name      string
 	Age       int       `storm:"index"`
 	CreatedAt time.Time `storm:"index"`
+	UpdatedAt NullTime  `storm:"index"`
 }
 
 func prepareDB() (string, *storm.DB) {
@@ -57,6 +100,7 @@ func prepareDB() (string, *storm.DB) {
 			Name:      name,
 			Age:       21 + i,
 			CreatedAt: time.Now(),
+			UpdatedAt: NullTime{time.Now().AddDate(0, -1*i, 0), true},
 		}
 		err := db.Save(&user)
 
